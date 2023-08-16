a999520b95e4a5e31ce93e08e35ec615dc8af0bd
Dave Smith <getdavemail@gmail.com>
2023-08-16 13:27:06 +0100

    Enable Rename option in block dropdown

--- packages/block-editor/src/hooks/index.js
+++ packages/block-editor/src/hooks/index.js
@@ -22,6 +22,7 @@ import './metadata';
 import './metadata-name';
 import './behaviors';
 import './custom-fields';
+import './block-rename-ui';
 
 export { useCustomSides } from './dimensions';
 export { useLayoutClasses, useLayoutStyles } from './layout';
